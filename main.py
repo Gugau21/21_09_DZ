25dc553a61252ac427571344e83ececc44a8ad8f
Nazar Bezmenov <nazarbezmenov1@gmail.com>
2023-09-20 22:33:18 +0100

    Fix bugs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import secrets
 import flask
 import requests
+import faker
 
 
 def generate_password(l):
@@ -12,15 +13,21 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
+    return 'Головна сторінка'
+
+@app.errorhandler(404)
+def page_not_found(error):
     return 'О ні, сторінка не знайдена!', 404
 
-@app.route("/requirements")
+@app.route("/requirements/", methods=['GET'])
 def requirements():
     txt = ''
     with open("requrements.txt") as file:
         for line in file:
             txt += line + '<br>'
     return txt
+
+
 
 @app.route('/generate-password', methods=['POST'])
 def generate_view():
@@ -38,9 +45,7 @@ def api_view():
     return
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return 'О ні, сторінка не знайдена!', 404
+
 
 
 if __name__ == "__main__":
